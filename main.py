a8d9a7320f08d3db3baf9a2200889dd74f6c2df7
cris <99284602+cristianocastle@users.noreply.github.com>
2024-09-18 02:47:07 +0000

    up

---
+++ main.py
@@ -0,0 +1,54 @@
+# cris padilla, Naithen Ramirez
+#
+
+
+import check_input
+
+
+
+def read_map():
+    hiddenmap = open("map.txt")
+    list2d = []
+    for row in hiddenmap:
+        list = []
+        for item in row:
+            if item != "" and item != '\n':
+                list.append(item)
+        list2d.append(list)
+    return list2d
+
+'''
+def display_map(map, player):
+    pass
+    map = open("shownmap.txt")
+    shownmap2d = []
+    for row in map:
+        list = []
+        for item in row:
+            if item != "" and item != '\n':
+                list.append(item)
+        shownmap2d.append(list)
+    return showmap2d
+    pass
+'''
+def move_player(player, dir, upper_bound):
+    pass 
+
+
+    if dir == 'W': 
+        player[1] += 1
+    if dir == 'A':
+        player[0] -= 1
+    if dir == 'S':
+        player[1] -= 1
+    if dir == 'D'
+        player[0] += 1
+
+
+def count_treasure_traps(map, player, upper_bound):
+    pass
+
+
+def main():
+    
+    player = [0,0]
