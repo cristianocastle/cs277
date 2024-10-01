a0bfda68683e747045b2c88421cb04ab3b99179e
NaithenR <ramireznaithen@gmail.com>
2024-10-01 10:18:30 -0700

    start

---
+++ lab6/die.py
@@ -0,0 +1,13 @@
+class Die:
+    def __init__(self, sides= 6):
+        pass
+    def roll(self):
+        pass
+    def __str__(self):
+        pass
+    def __lt__(self,other):
+        pass
+    def __eq__(self,other):
+        pass
+    def __sub__(self,other):
+        pass
---
+++ lab6/player.py
@@ -0,0 +1,13 @@
+class Player:
+    def __init__(self):
+        pass
+    def get_points(self):
+        pass
+    def roll_dice(self):
+        pass
+    def has_pair(self):
+        pass
+    def has_three_of_a_kind(self):
+        pass
+    def __str__(self):
+        pass
