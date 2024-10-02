a25e8c66cd4add343945aa46b5447e41c03b86ef
NaithenR <ramireznaithen@gmail.com>
2024-10-02 12:04:12 -0700

    update

--- lab6/player.py
+++ lab6/player.py
@@ -1,13 +1,31 @@
-class Player:
+from die import Die
+
+class Player(Die):
     def __init__(self):
-        pass
+        self.dice = [Die(),Die(),Die()]
+        self.dice.sort()
+        self.points = 0
+    
     def get_points(self):
-        pass
+        return self.points
+
     def roll_dice(self):
-        pass
+        for d in self.dice:
+            d.roll()
+   
     def has_pair(self):
-        pass
+        if self.dice[0] == self.dice[1] or self.dice[0] == self.dice[2] or self.dice[1] == self.dice[2]:
+            self.points += 1
+            return True
+        else:
+            return False
     def has_three_of_a_kind(self):
+        if self.dice[0] == self.dice[1] == self.dice[2]:
+            self.points += 3
+            return True
+        else:
+            return False
+    def has_series(self):
         pass
     def __str__(self):
         pass
