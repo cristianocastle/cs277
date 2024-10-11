b11c4da453b9ec83c1c089639c76a70800f35641
cris <99284602+cristianocastle@users.noreply.github.com>
2024-10-10 18:53:02 -0700

    update

--- lab7/caesar.py
+++ lab7/caesar.py
@@ -4,52 +4,14 @@ class CaesarCipher(Cipher):
     '''
     A cipher where each letter is shifted a certain number of places down the alphabet.
     '''
-    def __init__(self):
+    def __init__(self,shift):
         '''
         Initializes the CaesarCipher class with a default shift value of 3.
         '''
         super().__init__()
         self._shift = 3
-        
-    def encrypt_message(self,message):
-        '''
-        Encrypts the given message using the Caesar cipher.
-
-        Parameters:
-        message (str): The message to be encrypted.
-
-        Returns:
-        str: The encrypted message.
-        '''
-        message = message.upper()
-        _encrypted_message = ""
-        for char in message:
-            if char in self._alphabet:
-                _encrypted_message += self._encrypt_letter(char)
-            else:
-                _encrypted_message += char
-        return _encrypted_message
     
-    def decrypt_message(self,message):
-        '''
-        Decrypts the given message using the Caesar cipher.
-
-        Parameters:
-        message (str): The message to be decrypted.
-
-        Returns:
-        str: The decrypted message.
-        '''
-        message = message.upper()
-        _decrypted_message = ""
-        for char in message:
-            if char in self._alphabet:
-                _decrypted_message += self._decrypt_letter(char)
-            else:
-                _decrypted_message += char
-        return _decrypted_message
-    
-    def _encrypt_letter(self, letter):
+    def _encrypt_letter(self,letter):
         '''
         Encrypts a single letter using the Caesar cipher.
 
@@ -60,7 +22,7 @@ class CaesarCipher(Cipher):
         str: The encrypted letter.
         '''
         index = self._alphabet.index(letter)
-        return self._reverse_alphabet[(index + self._shift) % 26]
+        return self._alphabet[(index + self._shift) % 26]
     
     def _decrypt_letter(self,letter):
         '''
@@ -72,5 +34,5 @@ class CaesarCipher(Cipher):
         Returns:
         str: The decrypted letter.
         '''
-        index = self._reverse_alphabet.index(letter)
-        return self._alphabet[(index + self._shift) % 26]
+        index = self._alphabet.index(letter)
+        return self._alphabet[(index - self._shift) % 26]
--- lab7/main.py
+++ lab7/main.py
@@ -17,55 +17,59 @@ def main():
         print("2. Decrypt")
         decoder = get_int_range("Enter 1 or 2: ", 1, 2)
         
-        if decoder == 1: # Encrypt
+        if decoder == 1:  # Encrypt
             print('1. Atbash')
             print('2. Caesar')
             encryption_type = get_int_range("Enter 1 or 2: ", 1, 2)
             message = input("Enter message: ")
-            
-            if encryption_type == 1: # Atbash
+
+            if encryption_type == 1:  # Atbash
                 cipher = Cipher()
                 encrypted_message = cipher.encrypt_message(message)
-                
-            elif encryption_type == 2: # Caesar
-                shift = get_int_range("Enter shift value: ", 1, 25) 
-                cipher = CaesarCipher()
-                cipher._shift = shift 
+                shift = None  # No shift for Atbash
+
+            elif encryption_type == 2:  # Caesar
+                shift = get_int_range("Enter shift value: ", 1, 25)
+                cipher = CaesarCipher(shift)
                 encrypted_message = cipher.encrypt_message(message)
 
-            try: # Save encrypted message to file
-                with open('message.txt', 'w') as file: 
-#TODO
-                    file.write(encrypted_message) 
+            try:  # Save encrypted message and shift value to file
+                with open('message.txt', 'w') as file:
+                    file.write(f"{shift}\n{encrypted_message}")
                 print("Encrypted message saved to 'message.txt'.")
-            except: 
+            except:
                 print("Error saving encrypted message.")
-            
-            
-        elif decoder == 2: # Decrypt
+
+        elif decoder == 2:  # Decrypt
             print('1. Atbash')
             print('2. Caesar')
             decryption_type = get_int_range("Enter 1 or 2: ", 1, 2)
-        
-            try: # Read encrypted message from file
-                with open('message.txt', 'r') as file: 
-                    message = file.read()
+
+            try:  # Read encrypted message and shift value from file
+                with open('message.txt', 'r') as file:
+                    saved_shift = file.readline().strip()
+                    message = file.read().strip()
             except FileNotFoundError:
                 print("No encrypted message found to decrypt.")
                 continue
-                
-            if decryption_type == 1: # Atbash
+
+            if decryption_type == 1:  # Atbash
+                if saved_shift != 'None':
+                    print("Error: The saved message is not encrypted using Atbash.")
+                    continue
                 cipher = Cipher()
                 decrypted_message = cipher.decrypt_message(message)
                 print(f"Decrypted message: {decrypted_message}")
-                
-            elif decryption_type == 2: # Caesar
-                shift = get_int_range("Enter shift value: ", 1, 25) 
-                cipher = CaesarCipher()
-                cipher._shift = shift
+
+            elif decryption_type == 2:  # Caesar
+                shift = get_int_range("Enter shift value: ", 1, 25)
+                if saved_shift != str(shift):
+                    print("Error: The shift value does not match the one used for encryption.")
+                    continue
+                cipher = CaesarCipher(shift)
                 decrypted_message = cipher.decrypt_message(message)
-                print(f"Decrypted message: {decrypted_message}")           
-     
+                print(f"Decrypted message: {decrypted_message}")
+
 
 if __name__ == "__main__":
     main()
