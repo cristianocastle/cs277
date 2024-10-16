136808e72f35d42dbcda7585159b177a4b06744c
cris <99284602+cristianocastle@users.noreply.github.com>
2024-10-15 18:05:07 -0700

    update vehicle class

--- lab8/vehicle.py
+++ lab8/vehicle.py
@@ -1,36 +1,59 @@
 
-import random
 
-class Vehicle(): 
+import random
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
     
     def __init__(self, name, initial, min_speed, max_speed):
-        self.name = name
-        self.initial = initial
-        self.min_speed = min_speed
-        self.max_speed = max_speed
-        self.position = 0 
-        self.energy = 100
-    
+        self._name = name
+        self._initial = initial
+        self._min_speed = min_speed
+        self._max_speed = max_speed
+        self._position = 0 
+        self._energy = 100
+
+    @property
+    def initial(self):
+        return self._initial
+
+    @property
+    def position(self):
+        return self._position
+
+    @property
+    def energy(self):
+        return self._energy
+
     def fast(self, dist): 
-        if self.energy >= 5:
-            spaces_moved = random.randint(self.min_speed, self.max_speed)
-            self.energy -= 5
+        if self._energy >= 5:
+            spaces_moved = random.randint(self._min_speed, self._max_speed)
+            self._energy -= 5
             if spaces_moved > dist:
-                self.position += spaces_moved
+                self._position += spaces_moved
+                return f"{self._name} moved {spaces_moved} spaces and crashed into the obstacle."
             else:
-                self.position = dist - 1 
-        return self.name, self.position 
-                
-            
+                self._position = dist - 1
+                return f"{self._name} moved {spaces_moved} spaces."
+        else: 
+            return f"{self._name} does not have enough energy to move fast."
+
     def slow(self, dist): 
-        pass
-    
+        half_speed = (self._min_speed + self._max_speed) // 2
+        if half_speed > dist:
+            self._position = dist - 1
+            return f"{self._name} moved {half_speed} spaces and went around the obstacle."
+        else:
+            self._position += half_speed
+            return f"{self._name} moved {half_speed} spaces."
+
     def __str__(self):
-        pass
-    
+        return f"Vehicle: {self._name}, Position: {self._position}, Energy: {self._energy}"
+
+    @abstractmethod
     def description_string(self):
         pass
-    
+
+    @abstractmethod
     def special_move(self, dist):
         pass
-    
