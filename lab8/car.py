6becf28eb50a3dc8fa2715474f48da509d95e4b2
NaithenR <ramireznaithen@gmail.com>
2024-10-15 22:56:13 -0700

    car/truck first

--- lab8/car.py
+++ lab8/car.py
@@ -1,16 +1,24 @@
 
-
 from vehicle import Vehicle
-
+import random
 class Car(Vehicle): 
     
     def __init__(self):
-        super().__init__
+        super().__init__("Lighning Car", "C", 6, 8)
     
     def description_string(self):
-        pass
+        return f"Car Name: {self._name}, Initial: {self._initial}, Speed Range: {self._min_speed}-{self._max_speed}, Energy: {self._energy}"
     
     def special_move(self, dist):
-        pass
-    
-    
+        if self._energy >= 15:
+            speed = random.randint(self._min_speed, self._max_speed) * 1.
+            if speed >= dist:
+                self._position += dist
+                self._energy -= 15
+                return f"{self._name} used special move and traveled {dist} units to reach the obstacle."
+            else:
+                self._position += speed
+                self._energy -= 15
+                return f"{self._name} uses nitro boost and moves {int(speed)} units,"
+        else:
+            return f"{self._name} doesn't have enough energy for a special move."
--- lab8/truck.py
+++ lab8/truck.py
@@ -1,14 +1,24 @@
 
-
 from vehicle import Vehicle
-
+import random
 class Truck(Vehicle):
-    
     def __init__(self):
-        pass
-    
+        super().__init__("Behemoth Truck", "T", 4, 8)
+
     def description_string(self):
-        pass
+        return f"Car Name: {self._name}, Initial: {self._initial}, Speed Range: {self._min_speed}-{self._max_speed}, Energy: {self._energy}"
+
     
     def special_move(self, dist):
-        pass
+        if self._energy >= 15:
+            speed = random.randint(self._min_speed, self._max_speed) * 2
+            if speed >= dist:
+                self._position += dist
+                self._energy -= 15
+                return f"{self._name} rams forward bashing through the Obstacle to go {int(speed)} units."
+            else:
+                self._position += speed
+                self._energy -= 15
+                return f"{self._name} rams forward bashing through the Obstacle to go {int(speed)} units."
+        else:
+            return f"{self._name} doesn't have enough energy for a special move."
