8e26b1959ba564d07d29529ee86942c37a512b8f
cristianocastle <99284602+cristianocastle@users.noreply.github.com>
2024-10-30 16:34:14 -0700

    update

--- lab10/map.py
+++ lab10/map.py
@@ -1,15 +1,35 @@
 
 
-class Map(): 
+class Map():
+    """
+    A singleton class representing the game map.
+
+    Attributes:
+        _instance (Map): The singleton instance of the Map class.
+        _initialized (bool): A flag indicating whether the map has been initialized.
+        map (list): A 2D list representing the game map.
+        _revealed (list): A 2D list indicating which parts of the map have been revealed.
+    """
     
     _instance = None
     _initialized = False
+
     def __new__(cls):
+        """
+        Ensures that only one instance of the Map class is created (singleton pattern).
+
+        Returns:
+            Map: The singleton instance of the Map class.
+        """
         if cls._instance is None:
             cls._instance = super().__new__(cls)
         return cls._instance
     
     def __init__(self):
+        """
+        Initializes the map by reading from a file and setting up the revealed status.
+        This method is only executed once due to the singleton pattern.
+        """
         if not Map._initialized:
             self.map = []
             with open("map1.txt", "r") as file:
@@ -19,12 +39,36 @@ class Map():
             Map._initialized = True
     
     def __getitem__(self, row):
+        """
+        Allows access to the map rows using indexing.
+
+        Args:
+            row (int): The index of the row to access.
+
+        Returns:
+            list: The specified row of the map.
+        """
         return self.map[row]
     
     def __len__(self):
+        """
+        Returns the number of rows in the map.
+
+        Returns:
+            int: The number of rows in the map.
+        """
         return len(self.map)
     
-    def show_map(self,loc):
+    def show_map(self, loc):
+        """
+        Displays a 5x5 portion of the map centered around the hero's location.
+
+        Args:
+            loc (tuple): The (row, column) location of the hero.
+
+        Returns:
+            str: A string representation of the 5x5 portion of the map.
+        """
         map_display = ""
         hero_row, hero_col = loc
         for i in range(5):  
@@ -34,16 +78,3 @@ class Map():
                 elif 0 <= i < len(self.map) and 0 <= j < len(self.map[i]):
                     if self._revealed[i][j]:
                         map_display += self.map[i][j] + " "
-                    else:
-                        map_display += "x " 
-                else:
-                    map_display += "x "  
-            map_display += "\n"
-        return map_display.strip()
-    def reveal (self,loc):
-        row, col = loc
-        self._revealed[row][col] = True
-    def remove_at_loc(self, loc):
-        row, col = loc
-        self.map[row][col] = "n"
-        
