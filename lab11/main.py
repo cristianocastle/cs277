4bf322a5a5a265ef012f030fc07eb805fe8f44bb
cris <99284602+cristianocastle@users.noreply.github.com>
2024-11-05 19:46:27 -0800

    update

--- lab11/main.py
+++ lab11/main.py
@@ -7,9 +7,18 @@ from easy_door_factory import EasyDoorFactory
 from difficult_door_factory import DifficultDoorFactory
 
 def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+    print("Welcome to the Escape Room.")
+    print("You must unlock 3 doors to escape...")
+    difficulty = get_int_range("(1. Easy 2. Hard): ", 1, 2)
     
+    if difficulty == 1: # easy
+        factory = EasyDoorFactory() # create a factory
+        doors = [factory.create_door() for _ in range(3)] # create 3 doors
+    elif difficulty == 2: # hard
+        factory = DifficultDoorFactory() # create a factory
+        doors = [factory.create_door() for _ in range(3)] # create 3 doors
+        
+        
+        
+
     
