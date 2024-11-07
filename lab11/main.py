1c9c49e02b096981e18e91a7f7c1b581459a1e07
NaithenR <ramireznaithen@gmail.com>
2024-11-06 20:54:23 -0800

    Update main.py

--- lab11/main.py
+++ lab11/main.py
@@ -18,7 +18,20 @@ def main():
         factory = DifficultDoorFactory() # create a factory
         doors = [factory.create_door() for _ in range(3)] # create 3 doors
         
-        
-        
+    for i in range(3):
+        print(f"\nAttempting to open door {i+1}...")
+        door = doors[i]
+        print(door.examine_door())
 
-    
+        while not door.is_unlocked():
+            print(door.menu_options()) # display menu
+            choice = get_int_range("Enter your choice: ", 1, door.get_menu_max())    
+            print(door.attempt(choice))
+
+            if door.is_unlocked():
+                print(door.success())
+            else:
+                print(door.clue())
+    print("\nCongratulations! You escaped...this time.")
+if __name__ == "__main__":
+    main()
