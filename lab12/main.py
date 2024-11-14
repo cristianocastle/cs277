9aa19ebcf3d10eb775bce784efe35eadd270ce7e
cris <99284602+cristianocastle@users.noreply.github.com>
2024-11-13 18:59:37 -0800

    update

---
+++ lab12/main.py
@@ -0,0 +1,17 @@
+# naithen ramirez, cris padilla
+# 11/13/24
+# this program is a thanksgiving dinner program 
+
+
+def examine_plate(p): 
+    pass
+
+
+def main():
+    pass
+
+
+
+
+if __name__ == "__main__":
+    main()
