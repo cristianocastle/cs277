1372ac38c2c2106d84fd2d3becd3fd15392c8b39
cris <99284602+cristianocastle@users.noreply.github.com>
2024-11-19 19:06:08 -0800

    update

---
+++ lab13/main.py
@@ -0,0 +1,12 @@
+# naithen ramirez, cris padilla
+# 11/21/24
+# this program is a tasklist 
+
+def main_menu():
+    pass
+
+def get_date():
+    pass
+
+def get_time():
+    pass
---
+++ lab13/tasklist.py
@@ -0,0 +1,29 @@
+class Tasklist():
+    pass
+
+    def __init__(self):
+        pass
+    
+    def add_task(self, desc, date, time):
+        pass
+    
+    def get_current_task(self):
+        pass
+    
+    def mark_complete(self):
+        pass
+    
+    def postpone_task(self, date, time):
+        pass
+    
+    def save_file(self):
+        pass
+    
+    def __len__(self):
+        pass
+    
+    def __iter__(self):
+        pass
+    
+    def __next__(self):
+        pass
