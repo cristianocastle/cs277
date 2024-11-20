ffaa9f6b17526bae46c72f36f0d5dbf33b3b1d0e
cris <99284602+cristianocastle@users.noreply.github.com>
2024-11-19 19:14:59 -0800

    update

--- lab13/main.py
+++ lab13/main.py
@@ -2,8 +2,17 @@
 # 11/21/24
 # this program is a tasklist 
 
+from check_input import get_int_range
+
 def main_menu():
-    pass
+    print("1. Display all tasks")
+    print("2. Display current task")
+    print("3. Add new task")
+    print("4. Mark current task complete")
+    print("5. Postpone current task")
+    print("6. Search tasks by date")
+    print("7. Save and quit")
+    return get_int_range("Enter your choice: ", 1, 7)
 
 def get_date():
     pass
