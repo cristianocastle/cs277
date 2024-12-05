17d55934c3938c30436ca75617b826ed30280618
NaithenR <ramireznaithen@gmail.com>
2024-12-05 10:30:11 -0800

    Update state_asleep.py

--- lab 14/state_asleep.py
+++ lab 14/state_asleep.py
@@ -1,1 +1,14 @@
-# TODO
+from puppy_state import PuppyState
+import state_eat
+
+
+class StateAsleep(PuppyState):
+    def feed(self, puppy):
+        puppy.change_state(state_eat.StateEat())
+        puppy.reset()
+        puppy.inc_feeds()
+        return "The puppy wakes up and comes running to eat."
+
+    def play(self, puppy):
+        return "The puppy is asleep. It doesn't want to play right now."
+    
