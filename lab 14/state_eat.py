331cea185420b142fff0f41803a4e36639f8d643
NaithenR <ramireznaithen@gmail.com>
2024-12-05 10:31:10 -0800

    Update state_eat.py

--- lab 14/state_eat.py
+++ lab 14/state_eat.py
@@ -1,1 +1,19 @@
-# TODO
+from puppy_state import PuppyState
+import state_asleep
+import state_play
+
+class StateEat(PuppyState):
+    def feed(self, puppy):
+        puppy.inc_feeds()
+        if puppy.feeds >= 3:
+            puppy.change_state(state_asleep.StateAsleep())
+            return (
+                "The puppy continues to eat as you add another scoop of kibble to its bowl.\n"
+                "The puppy ate so much it fell asleep.")
+        return "The puppy continues to eat as you add another scoop of kibble to its bowl."
+
+    def play(self, puppy):
+        puppy.change_state(state_play.StatePlay())
+        puppy.reset()
+        puppy.inc_plays()
+        return "The puppy looks up from its food and chases the ball you threw."
